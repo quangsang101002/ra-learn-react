ea18b67d5667ee53fa7a618f9ba7e49207f6ae1d
abc <abc@gmail.com>
2023-05-31 21:06:09 +0700

    ss2 ví dụ event

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { studentName: 'Sang' };
-    this.changeStudentName = this.changeStudentName.bind(this);
+    this.state = { text: 'hello' };
   }
-  changeStudentName() {
-    this.setState({ studentName: 'Quang' });
+  handelClick(value) {
+    alert(value);
+  }
+  handelClick3() {
+    const { text } = this.state;
+    // alert(this.state.text);
+    alert(text);
   }
   render() {
     return (
-      <div className="App">
-        studentName: {this.state.studentName}
-        <button onClick={this.changeStudentName}>Click</button>
-      </div>
+      <>
+        <h1>ví dụ event</h1>
+        <button onClick={() => this.handelClick('Button o1 click 01')}>
+          Button 01
+        </button>
+        <button onClick={() => this.handelClick('Button o1 click 02')}>
+          Button 02
+        </button>
+        <button onClick={() => this.handelClick3()}>Button 03</button>
+      </>
     );
   }
 }
