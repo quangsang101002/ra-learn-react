b05cda43d4748e4640ae841518518eaa01f0c34e
abc <abc@gmail.com>
2023-05-31 20:37:00 +0700

    session02-thay đổi state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { studentName: 'Quang' };
+    this.state = { studentName: 'Sang' };
+    this.changeStudentName = this.changeStudentName.bind(this);
+  }
+  changeStudentName() {
+    this.setState({ studentName: 'Quang' });
   }
   render() {
-    return <div className="App">studentName: {this.state.studentName}</div>;
+    return (
+      <div className="App">
+        studentName: {this.state.studentName}
+        <button onClick={this.changeStudentName}>Click</button>
+      </div>
+    );
   }
 }
 export default App;
